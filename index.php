c7eeab49334aebb4af68d9ed3a8931e7c5835d61
Samir <samir.guitar@gmail.com>
2016-10-18 00:52:07 -0300

    Middleware de Rota

--- index.php
+++ index.php
@@ -6,21 +6,15 @@ require 'vendor/autoload.php';
 
 $app = new Application();
 
-$app->before(function(){
-  print 'Conteúdo before<br/>';
-});
-
 $app->get('/', function(Application $app){
   // return $app->escape('Middlewares' ." - ". $data);
   return $app->escape("Middlewares") . "<br/>";
-});
-
-$app->after(function(){
+})
+->before(function(){
+  print 'Conteúdo before<br/>';
+})
+->after(function(){
   print "After<br/>";
-});
-
-$app->finish(function(){
-  print("Finish<br/>");
 });
 
 $app->run();
