d9b5f89a517e029c2a2fbaa34350c601dbe23ee7
Samir <samir.guitar@gmail.com>
2016-10-18 02:40:10 -0300

    Modificação da pasta ESTUDO arquivo INDEX

--- estudo/index.php
+++ estudo/index.php
@@ -6,15 +6,24 @@ require 'vendor/autoload.php';
 
 $app = new Application();
 
-$app->get('/', function(Application $app){
-  // return $app->escape('Middlewares' ." - ". $data);
-  return $app->escape("Middlewares") . "<br/>";
+$app->get('/users/{id}', function($id){
+  if(is_null($id))
+    return "id_Inexistente"."<br/>";
+
+  return "Meu id é:" . $id."<br/>";
 })
-->before(function(){
-  print 'Conteúdo before<br/>';
-})
-->after(function(){
+->value('id',null);
+
+$app->before(function(){
+  print "Conteúdo before<br/>";
+});
+
+$app->after(function(){
   print "After<br/>";
+});
+
+$app->finish(function(){
+  print 'Conteúdo finish<br/>';
 });
 
 $app->run();
