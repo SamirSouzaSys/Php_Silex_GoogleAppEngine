634adcc61c3e402cf90c2c6bc1e04060e4ea5421
Samir <samir.guitar@gmail.com>
2016-10-17 23:38:09 -0300

    Valores Default em rotas dinâmicas

--- index.php
+++ index.php
@@ -7,7 +7,10 @@ require 'vendor/autoload.php';
 $app = new Application();
 
 $app->get('/users/{id}', function($id){
+  if(is_null($id))
+    return "Parameter invalid";
+
   return 'User ID: '. $id;
-});
+})->value('id', null);
 
 $app->run();
